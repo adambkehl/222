4742f8ed495ce9caface82413dc256f53024f70b
Adam Kehl <me@adamkehl.com>
2019-02-22 03:56:51 -0800

    Create Sudoku.h

---
+++ Sudoku/Sudoku.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+#define SIZE 9
+
+class Sudoku {
+private:
+	int **grid;
+public:
+	Sudoku();
+	~Sudoku();
+	void genPuzzleFromFile(char *fname);
+	void printPuzzle();
+	bool const isValidRow(int x, int y, int value);
+	bool const isValidCol(int x, int y, int value);
+	bool const isValidBox(int x, int y, int value);
+	bool const isValid(int x, int y, int value);
+	bool const solve();
+};
