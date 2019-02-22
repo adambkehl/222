a73730ff856e0451569883ad3fde9c5472608302
Adam Kehl <me@adamkehl.com>
2019-02-22 03:48:36 -0800

    Create page-303-7.cpp

---
+++ Assignment #2/page-303-7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string words = "";
+	int vowels = 0, consonants = 0;
+	char c;
+	cout << "Enter words (q to quit): ";
+	do {
+		cin >> words;
+
+		if (isalpha(c = words[0])) {
+			if (c == 'a' || c == 'e' || c == 'i' ||
+				c == 'o' || c == 'u' || c == 'A' ||
+				c == 'E' || c == 'I' || c == 'O' ||
+				c == 'U')
+				vowels++;
+			else consonants++;
+		}
+	} while (words != "q");
+	consonants--;
+
+	cout << endl << "Vowels: " << vowels << endl << "Consonants: " << consonants << endl;
+
+	cin.get(), cin.get();
+}
