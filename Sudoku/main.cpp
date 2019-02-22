286cd92dfe98eec1d0b4855e54978248ac83345d
Adam Kehl <me@adamkehl.com>
2019-02-22 04:06:48 -0800

    Update main.cpp

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -1,14 +1,7 @@
 #include "Sudoku.h"
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	Sudoku puzzle;
-	/*
-	if (argc == 1)
-		puzzle.genPuzzle(1);
-	else*/
-
-	puzzle.genPuzzleFromFile(argv[1]);
-	
+	puzzle.genPuzzleFromFile(argv[1]);	
 	puzzle.printPuzzle();
 	puzzle.solve();
 	puzzle.printPuzzle();
