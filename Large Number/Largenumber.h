4e9588361b72dc7c08860816589f6ddfc4e9a8fb
Adam Kehl <me@adamkehl.com>
2019-02-26 23:35:46 -0800

    Update Largenumber.h

--- Large Number/Largenumber.h
+++ Large Number/Largenumber.h
@@ -16,16 +16,16 @@ public:
 	Largenumber(char const *);
 	~Largenumber();
 	Largenumber(Largenumber const &); //copy constructor
-	Largenumber operator+(Largenumber const &);
+	Largenumber operator+(Largenumber const &n);
 	Largenumber &operator-(Largenumber const &);
 	Largenumber &operator++();
 	Largenumber &operator++(int);
 	Largenumber &operator--();
 	Largenumber &operator--(int);
-	Largenumber &operator=(Largenumber const &);
-	bool &operator==(Largenumber const &) const;
-	bool &operator!=(Largenumber const &) const;
+	Largenumber operator=(Largenumber const &);
+	bool operator==(Largenumber const &) const;
+	bool operator!=(Largenumber const &) const;
 	int length(int const) const;
-	friend istream const &operator>>(istream const &, Largenumber const &);
-	friend ostream const &operator<<(istream const &, Largenumber const &);
+	//friend istream const &operator>>(istream const &, Largenumber const &);
+	//friend ostream const &operator<<(istream const &, Largenumber const &);
 };
